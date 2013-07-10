929d1320cd345db81bc3ad21b80991580a815e5d
Ulrich Reining <spuelrich@gmx.net>
2013-07-10 22:04:26 +0200

    fertig

--- BesserStadtRadeln.user.js
+++ BesserStadtRadeln.user.js
@@ -4,123 +4,84 @@
 // @include     https://stadtradeln.de/index.php?id=167*
 // @include     https://www.stadtradeln.de/index.php?id=167*
 // @require     https://stadtradeln.de/typo3conf/ext/sexybookmarks/res/jquery/js/jquery-1.8.2.min.js
-// @version     14
+// @version     15
 // ==/UserScript==
 
-/*
-var comments = {};
-$("#content tr")
-    .find("td:eq(3)")
-    .each(function(){comments[$(this).text()] = 1;});
 
-var comments_arr = [];
-for(var i in comments) {
-    comments_arr.push(i);
-}
-
-
-var $veryfirstrow = $("#content form table tr:first");
-
-var $originalinput = $("#content form table tr:last");
-
-$originalinput
-    .insertBefore($veryfirstrow)
-    .hide();
-
-var input_row_counter = 0;
-function moreinput() {
-    var $clone = $originalinput.clone();
-    $clone
-        .insertBefore($originalinput)
-        .show();
-
-    $clone.find("input[type=submit]").remove();
-    $clone.find("td:last").attr("id", "poststate" + input_row_counter);
-
-    $clone.find("input")
-        .each(function(){this.name = this.name + input_row_counter;
-                         this.id = this.name;
-                        });
-    $clone.find("input:eq(3)")
-        .autocomplete({source: comments_arr});
-    input_row_counter++;
-}
-
-for (var i=0;i<5;i++) {
-    moreinput();
-}
-$("#content form table tr:last").insertBefore("#content form table tr:first");
-
-var theurl = document.location.hostname.match("www") ? "http://www.stadtradeln.de/index.php?id=167" : "http://stadtradeln.de/index.php?id=167";
-
-
-var $sendrow = $('<tr><td id="bettersend"/></tr>')
-    .insertAfter($originalinput);
-
-function sendstadtradeln (ev) {
-    ev.preventDefault();
-    var data_fields = ["sr_new_entry_date", "sr_new_route_time", "sr_new_route_comment", "sr_new_route_distance"];
-
-    var running_posts = [];
-    for (var i=0;i<input_row_counter;i++) {
-        var pdata =
-            {sr_action: "add_data",
-             sr_new_route_persons: 1
-            };
-
-        for (var idx in data_fields) {
-            pdata[data_fields[idx]] = $("#" + data_fields[idx] + i).val();
-        }
-
-        if (pdata["sr_new_route_distance"]) {
-            running_posts[i] = 1;
-            $("#poststate"+i).html("running");
-            $.ajax({type: 'POST',
-                    url: theurl,
-                    data: pdata,
-                    dataType: 'text',
-                    context: {index: i},
-                    success: function() {
-                        running_posts[this.index] = 0;
-                        $("#poststate"+this.index).html("done");
-                        if ($.inArray(1, running_posts) < 0 ) {
-                            document.location = theurl;
-                        }
-                    },
-                   });
-        }
-    }
-}
-
-$('<a href="#">mehr</a>')
-    .appendTo("#bettersend")
-    .click(moreinput);
-
-$('<a href="#">absenden</a>')
-    .css({border: "solid black 1px",
-          padding: "2px",
-          background: "yellow"})
-    .appendTo("#bettersend")
-    .click(sendstadtradeln);
-*/
+var theurl = document.location.hostname.match("www") ? "https://www.stadtradeln.de/index.php?id=167" : "https://stadtradeln.de/index.php?id=167";
+var data_fields = ["sr_new_entry_date", "sr_new_route_time", "sr_new_route_persons", "sr_new_route_comment", "sr_new_route_distance", "sr_new_entry_id"];
 
 function input_changed () {
     $(this)
         .siblings(':last')
         .text('geändert')
-        .css('background-color', '#eecccc')
+        .css('background-color', '#ffff00')
         .parent().addClass('besser_changed');
 }
 
-function do_besser_save () {
+var running_posts = 0;
+function do_besser_save (ev) {
+    ev.preventDefault();
+
+    function ajax_update () {
+        var pdata = {};
+
+        var $cells = $('td', this);
+        var statusID = $cells.eq(6).attr('id');
+        for (var i=0; i<=5; i++) {
+            pdata[data_fields[i]] = $cells.eq(i).children('input').val();
+        }
+        if (pdata.sr_new_route_persons==undefined || pdata.sr_new_route_persons == '') {
+            pdata.sr_new_route_persons = 1;
+        }
+        if (parseInt(pdata.sr_new_entry_id) > 0) {
+            pdata.sr_action = 'edit_data';
+            }
+        else {
+            delete pdata.sr_new_entry_id;
+            pdata.sr_action = 'add_data';
+        }
+
+        $('#'+statusID).css('background-color', '#ff8800').text('speichern...');
+
+        running_posts++;
+        $.ajax({type: 'POST',
+                url: theurl,
+                data: pdata,
+                dataType: 'text',
+                success: function() {
+                    $('#'+statusID).css('background-color', '#00ff00').text('gespeichert');
+                    if (--running_posts == 0) {
+                        window.setTimeout(function(){document.location = theurl}, 1000);
+                    }
+                }
+               });
+    }
+    $('.besser_changed').each(ajax_update);
+}
+var $originalinput;
+var $table;
+var moreInputCounter = 0;
+
+function moreInput (ev) {
+    if (ev) {
+        ev.preventDefault();
+    }
+    var $zeile = $originalinput.clone();
+    $('input[type=submit]', $zeile).remove();
+    $('td input', $zeile)
+        .prop('name', '')
+        .prop('id', '');
+    $zeile.append('<td/>', $('<td class="status">NEU</td>').attr('id', 'bessernew' + ++moreInputCounter));
+    $zeile.addClass('besser_new').appendTo($table);
 }
 
 $(function() {
-    var $originalinput = $('#radelkalender_form table tr:last');
+    $originalinput = $('#radelkalender_form table tr:last');
     $('ul.socials').remove();
     $('iframe').remove();
 
-    var $table = $('<table></table>');
+    $table = $('<table></table>');
     $table.insertBefore('form#radelkalender_form');
 
     $('tr')
@@ -135,9 +96,9 @@ $(function() {
         .each(function() {
             var $zeile = $originalinput.clone()
             $('input[type=submit]', $zeile).remove();
-            $zeile.append('<td><input type="text"/></td><td class="status">OK</td>');
-
+            $zeile.append('<td><input type="text"/></td>');
             var $cells = $('td', this);
+            var ID;
             for (var i=0;i<=5;i++) {
                 var $input = $('td input', $zeile).eq(i);
                 $input
@@ -154,30 +115,29 @@ $(function() {
                     // ID einfügen
                     $('a:eq(1)', $cells.eq(i)).prop('href').match(/entry_id=([0-9]+)/);
                     val = RegExp.$1;
+                    ID = val;
+                    $input.prop('disabled', true);
                 }
                 else {
                     val = $cells.eq(i).text();
                 }
                 $input.val(val);
             }
+            $zeile.append($('<td class="status">OK</td>').attr('id', 'besserupdate'+ID));
 
             $zeile.addClass('besser_update').appendTo($table);
 
         });
     for (var i=0;i<=5;i++) {
-        var $zeile = $originalinput.clone();
-        $('input[type=submit]', $zeile).remove();
-        $('td input', $zeile)
-            .prop('name', '')
-            .prop('id', '');
-        $zeile.append('<td/><td class="status">NEU</td>');
-        $zeile.addClass('besser_new').appendTo($table);
+        moreInput();
     }
 
     $('tr.besser_update td').change(input_changed);
     $('tr.besser_new td').change(input_changed);
 
-    $table.append('<tr><td><a id="besser_save" href="#">speichern</a></td></tr>');
+    $('<div><a id="besser_save" href="#">speichern</a>&nbsp;<a id="besser_more" href="#">mehr Zeilen für Neueingabe</a></div>')
+        .insertAfter($table);
     $('#besser_save').click(do_besser_save);
+    $('#besser_more').click(moreInput);
 
 });
